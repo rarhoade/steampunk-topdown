79426df7dd446a6056306b7cb5f00f11788c6bb1
rarhoade <rarhoade@umich.edu>
2020-06-30 19:59:12 -0600

    swappable_skill_keys: added delegate association to alpha keys in PlayerCombat

--- Assets/Scripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerCombat.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections; using System.Collections.Generic;
+﻿using System.Collections; 
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerCombat : MonoBehaviour
 {
@@ -10,10 +12,18 @@ public class PlayerCombat : MonoBehaviour
     public GameObject sampleSkill;
     public Transform ShotOrigin;
     public Character c;
+    public delegate void SkillAction(Transform shot);
+    public static List<SkillAction> SkillSlots;
 
+    private int skillSlotSize = 4;
     void Start(){
         rb = GetComponent<Rigidbody2D>();
         c = GetComponent<Character>();
+        SkillSlots = new List<SkillAction>();
+        for(int i = 0; i < skillSlotSize; i++){
+            SkillSlots.Add(null);
+        }
+        SkillSlots[0] += FireBallBlast;
     }
     // Start is called before the first frame update
     void Update()
@@ -22,9 +32,21 @@ public class PlayerCombat : MonoBehaviour
             Attack();
         }       
         if(Input.GetMouseButtonDown(1) && canAttack){
-            Instantiate(sampleSkill, ShotOrigin.transform.position, ShotOrigin.rotation).
+            /*Instantiate(sampleSkill, ShotOrigin.transform.position, ShotOrigin.rotation).
                 GetComponent<ProjectileSkill>().
-                Shoot(c.GetPlayerNumber());
+                Shoot(c.GetPlayerNumber());*/
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha1)){
+            SkillSlots[0](ShotOrigin);
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha2)){
+            SkillSlots[1](ShotOrigin);
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha3)){
+            SkillSlots[2](ShotOrigin);
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha4)){
+            SkillSlots[3](ShotOrigin);
         }
     }
 
@@ -35,5 +57,11 @@ public class PlayerCombat : MonoBehaviour
 
     public void SetAnimator(Animator newAnimator){
         weaponAnimator = newAnimator;
+    }
+
+    void FireBallBlast(Transform shot){
+        Instantiate(sampleSkill, shot.transform.position, shot.rotation).
+            GetComponent<ProjectileSkill>().
+            Shoot(c.GetPlayerNumber());
     }
 }
