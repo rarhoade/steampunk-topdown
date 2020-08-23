63c9c9d472bb7f1de0535f45fdf2f2af7e5e0ed1
Larissa <larislu@umich.edu>
2020-08-23 19:50:44 -0400

    Added a teleport skill

---
+++ Assets/Prefabs/SkillPrefabs/Teleport/Teleport.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName="SkillBook/Teleport")]
+public class Teleport : ActiveSkill
+{
+    public ActiveSkill referenceSkill;
+    public float distance;
+
+    public override void ExecuteAction(Transform origin) {
+        Rigidbody2D rb2d = origin.transform.root.gameObject.GetComponent<Rigidbody2D>();
+
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = new Vector2 (
+            mousePosition.x - origin.transform.position.x,
+            mousePosition.y - origin.transform.position.y
+        );
+
+        Vector2 newPos = (Vector2)origin.transform.position + (distance * direction.normalized);
+        rb2d.position = newPos;
+    }
+}
